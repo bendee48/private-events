3e665da2d6c4ac279b3681afa5342a1253aee49f
bendee <benbanj@hotmail.com>
2026-03-11 01:09:21 +0000

    Adding button to unattend an event
    - Adding button and passing local variable to event partial for button to unattend an event- Adding a helper method to Event to pass that events attendance instance for a particular user

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -2,7 +2,8 @@ class Attendance < ApplicationRecord
   belongs_to :user
   belongs_to :event
 
-  validate :user_already_attending?
+  # With the new status system this validation is redundant for now
+  # validate :user_already_attending?
 
   def user_already_attending?
     if Attendance.exists?(user_id: user_id, event_id: event_id, status: "going")
--- app/models/event.rb
+++ app/models/event.rb
@@ -9,11 +9,13 @@ class Event < ApplicationRecord
   scope :upcoming, -> { where("date >= ?", Time.now) }
   scope :past,     -> { where("date < ?", Time.now) }
 
-  # def guests
-  #   User.joins(:attendances).where(attendances: { status: "going", event_id: id })
-  # end
 
   def created_by?(user)
     user_id == user.id
+  end
+
+  # Returns the Attendance record associated with this event and a provided user
+  def attendance_for(user)
+    attendances.find_by(user_id: user.id)
   end
 end
